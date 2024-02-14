23949b9d84cbec5a3ddf176fe8b0f3ccc0f49fe9
table <156590657+table106@users.noreply.github.com>
2024-02-14 06:54:45 +0100

    Added the quality of life library
    its really useful

---
+++ qol.py
@@ -0,0 +1,11 @@
+def intInput(string: str) -> str:
+    return int(input(string))
+
+def isPrime(num: int):
+    from math import sqrt
+    if num < 2: # if num isn't "prime-or-not"able
+        return None
+    for i in range(sqrt(num), 1, -1):
+        if num % i == 0:
+            return False
+    return True
