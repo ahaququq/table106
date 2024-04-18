e164df1db24fcd0230f2cf2e52814618fa27cbbc
table <156590657+table106@users.noreply.github.com>
2024-04-18 10:39:56 +0200

    Update qol.py
    any iterable

--- qol.py
+++ qol.py
@@ -1,23 +1,17 @@
-def intInput(string: str) -> str:
+from typing import Any, Iterable
+from math import sqrt
+
+def intInput(string: Any) -> str:
     return int(input(string))
 
 def isPrime(num: int) -> bool:
-    from math import sqrt
-    if num < 2: # if num isn't "prime-or-not"able
-        return None
     for i in range(sqrt(num), 1, -1):
         if num % i == 0:
             return False
     return True
 
-def retype(value, type):
-    return type(value)
+def intify(value: Any) -> None:
+    value = int(value)
 
-def reverse(list: list) -> None:
-    list = list[::-1]
-
-def swap(a,b) -> None:
-    a, b = b, a
-
-def stringListToIntList(st: str, sep: str=" "):
-    st = list(map(int, st.split(sep)))
+def reverse(value: Iterable) -> None:
+    value = value[::-1]
